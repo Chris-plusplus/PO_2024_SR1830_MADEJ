dc72e6cda4715205dc732121326f67d5c51ea033
Chris-plusplus <72704303+Chris-plusplus@users.noreply.github.com>
2024-10-16 19:26:47 +0200

    Update OptionsParser.java

--- oollab/src/main/java/agh/ics/oop/OptionsParser.java
+++ oollab/src/main/java/agh/ics/oop/OptionsParser.java
@@ -8,8 +8,7 @@ public class OptionsParser {
     public static MoveDirection[] parse(String[] args){
         MoveDirection[] result = new MoveDirection[args.length];
         int counter = 0;
-        for(int i = 0; i != args.length; i++){
-            var arg = args[i];
+        for(var arg : args){
             switch (arg){
                 case "f": result[counter++] = MoveDirection.FORWARD; break;
                 case "b": result[counter++] = MoveDirection.BACKWARD; break;
