7763a9b8f7128200f3edc6e20fd57d7eeeeec368
Chris-plusplus <72704303+Chris-plusplus@users.noreply.github.com>
2024-10-23 19:50:18 +0200

    Update World.java

--- oollab/src/main/java/agh/ics/oop/World.java
+++ oollab/src/main/java/agh/ics/oop/World.java
@@ -1,11 +1,14 @@
 package agh.ics.oop;
 
+import agh.ics.oop.model.Animal;
 import agh.ics.oop.model.MapDirection;
 import agh.ics.oop.model.MoveDirection;
 import agh.ics.oop.model.Vector2d;
 
+import java.util.List;
+
 public class World {
-    private static void run(MoveDirection[] directions){
+    private static void run(List<MoveDirection> directions){
         System.out.println("Start");
         for(var arg : directions){
             switch (arg){
@@ -18,6 +21,15 @@ public class World {
         System.out.println("Stop");
     }
     public static void main(String[] args){
+        var animal = new Animal();
+        System.out.println(animal);
+        System.out.println();
+
+        List<MoveDirection> directions = OptionsParser.parse(args);
+        List<Vector2d> positions = List.of(new Vector2d(2,2), new Vector2d(3,4));
+        Simulation simulation = new Simulation(positions, directions);
+        simulation.run();
+
         Vector2d position1 = new Vector2d(1,2);
         System.out.println(position1);
         Vector2d position2 = new Vector2d(-2,1);
