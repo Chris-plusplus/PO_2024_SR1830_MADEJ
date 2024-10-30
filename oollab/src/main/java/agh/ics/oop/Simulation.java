8c0aaf4471d13fe354938a9225b331e0f50a93ad
Chris-plusplus <72704303+Chris-plusplus@users.noreply.github.com>
2024-10-30 19:48:03 +0100

    Simulation uses WorldMap

--- oollab/src/main/java/agh/ics/oop/Simulation.java
+++ oollab/src/main/java/agh/ics/oop/Simulation.java
@@ -3,27 +3,39 @@ package agh.ics.oop;
 import agh.ics.oop.model.Animal;
 import agh.ics.oop.model.MoveDirection;
 import agh.ics.oop.model.Vector2d;
+import agh.ics.oop.model.WorldMap;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class Simulation {
-    private List<Animal> animals = new ArrayList<>();
-    private List<MoveDirection> moves;
+    private final List<Animal> animals = new ArrayList<>();
+    private final List<MoveDirection> moves;
+    private final WorldMap worldMap;
 
-    public Simulation(List<Vector2d> initialPositions, List<MoveDirection> moves){
+    private static final String ANIMAL_LABEL = "Zwierzę";
+
+    public Simulation(List<Vector2d> initialPositions, List<MoveDirection> moves, WorldMap worldMap){
+        this.worldMap = worldMap;
+
         // List<T> można modyfikować, niezbędna lokalna kopia
         this.moves = new ArrayList<>(moves);
         for(var initialPosition : initialPositions){
             animals.add(new Animal(initialPosition));
+            worldMap.place(animals.getLast());
         }
+        System.out.println(worldMap.isOccupied(new Vector2d(2, 2)));
+        System.out.println(worldMap.isOccupied(new Vector2d(3, 4)));
     }
 
     public void run(){
+        System.out.println(worldMap);
         for(int i = 0; i != moves.size(); ++i){
             var animal = animals.get(i % animals.size());
-            animal.move(moves.get(i));
-            System.out.println("Zwierzę %d: %s".formatted(i % animals.size(), animal));
+            String animalStrBefore = animal.toLongString();
+            worldMap.move(animal, moves.get(i));
+            System.out.println("%s %d: {%s} -> {%s}".formatted(ANIMAL_LABEL, i % animals.size(), animalStrBefore, animal.toLongString()));
+            System.out.println(worldMap);
         }
     }
 }
