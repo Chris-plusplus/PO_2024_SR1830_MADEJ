782aa8c819fad20bacd86313b2a21ca534b8c594
Chris-plusplus <72704303+Chris-plusplus@users.noreply.github.com>
2024-11-26 17:08:00 +0100

    Update World.java

--- oollab/src/main/java/agh/ics/oop/World.java
+++ oollab/src/main/java/agh/ics/oop/World.java
@@ -24,7 +24,14 @@ public class World {
 
         WorldMap worldMap = new GrassField(5);
 
-        List<MoveDirection> directions = OptionsParser.parse(args);
+        List<MoveDirection> directions;
+        try{
+             directions = OptionsParser.parse(args);
+        }
+        catch (IllegalArgumentException e){
+            System.out.println(e.getMessage());
+            return;
+        }
         List<Vector2d> positions = List.of(new Vector2d(2,2), new Vector2d(3,4));
         Simulation simulation = new Simulation(positions, directions, worldMap);
         simulation.run();
