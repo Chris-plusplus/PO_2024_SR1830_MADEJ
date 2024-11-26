251a5bc5886e38ca5d6bcdfc37209957765d08ed
Chris-plusplus <72704303+Chris-plusplus@users.noreply.github.com>
2024-11-26 17:06:27 +0100

    Split OptionsParserTest's tests

--- oollab/src/test/java/agh/ics/oop/OptionsParserTest.java
+++ oollab/src/test/java/agh/ics/oop/OptionsParserTest.java
@@ -6,14 +6,41 @@ import org.junit.jupiter.api.Test;
 
 public class OptionsParserTest {
     @Test
-    public void parseTest() {
-        String[] args = {"f", "b", "l", "r", "forward", "up", "BACKWARD", "b", "r", "r", "l", "f"};
+    public void parseTestValid() {
+        String[] args = {"f", "b", "l", "r", "f", "b", "b", "b", "r", "r", "l", "f"};
         var results = OptionsParser.parse(args);
-        MoveDirection[] expected = {MoveDirection.FORWARD, MoveDirection.BACKWARD, MoveDirection.LEFT, MoveDirection.RIGHT, MoveDirection.BACKWARD, MoveDirection.RIGHT, MoveDirection.RIGHT, MoveDirection.LEFT, MoveDirection.FORWARD};
+        MoveDirection[] expected = {
+                MoveDirection.FORWARD,
+                MoveDirection.BACKWARD,
+                MoveDirection.LEFT,
+                MoveDirection.RIGHT,
+                MoveDirection.FORWARD,
+                MoveDirection.BACKWARD,
+                MoveDirection.BACKWARD,
+                MoveDirection.BACKWARD,
+                MoveDirection.RIGHT,
+                MoveDirection.RIGHT,
+                MoveDirection.LEFT,
+                MoveDirection.FORWARD
+        };
 
         Assertions.assertEquals(results.size(), expected.length);
         for(int i = 0; i != results.size(); ++i){
             Assertions.assertEquals(results.get(i), expected[i]);
         }
+    }
+
+    @Test
+    public void parseTestInvalid() {
+        Assertions.assertThrowsExactly(IllegalArgumentException.class, () -> {
+            String[] args = {"f", "b", "l", "r", "forward", "up", "BACKWARD", "b", "r", "r", "l", "f"};
+            var results = OptionsParser.parse(args);
+            MoveDirection[] expected = {MoveDirection.FORWARD, MoveDirection.BACKWARD, MoveDirection.LEFT, MoveDirection.RIGHT, MoveDirection.BACKWARD, MoveDirection.RIGHT, MoveDirection.RIGHT, MoveDirection.LEFT, MoveDirection.FORWARD};
+
+            Assertions.assertEquals(results.size(), expected.length);
+            for(int i = 0; i != results.size(); ++i){
+                Assertions.assertEquals(results.get(i), expected[i]);
+            }
+        });
     }
 }
