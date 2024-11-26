37d300ca776cc5eef7de160991a98f1149ded49b
Chris-plusplus <72704303+Chris-plusplus@users.noreply.github.com>
2024-11-26 17:07:34 +0100

    Update OptionsParser.java

--- oollab/src/main/java/agh/ics/oop/OptionsParser.java
+++ oollab/src/main/java/agh/ics/oop/OptionsParser.java
@@ -7,6 +7,8 @@ import java.util.Arrays;
 import java.util.List;
 
 public class OptionsParser {
+    private static final String ERROR_MESSAGE = "is an illegal move";
+
     public static List<MoveDirection> parse(String[] args){
         List<MoveDirection> result = new ArrayList<MoveDirection>();
         int counter = 0;
@@ -16,6 +18,7 @@ public class OptionsParser {
                 case "b" -> result.add(MoveDirection.BACKWARD);
                 case "r" -> result.add(MoveDirection.RIGHT);
                 case "l" -> result.add(MoveDirection.LEFT);
+                default  -> throw new IllegalArgumentException("%s %s".formatted(arg, ERROR_MESSAGE));
             }
         }
 
